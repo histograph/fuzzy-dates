3a2ef35014329023a8dcd74ffe88e2429077081f
Bert Spaan <bert.spaan@gmail.com>
2015-08-09 22:14:16 +0200

    ±±±± @wires, speciaal voor jou, vanaf Camping de Kooi ⛺

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ var validate = validator('fuzzy-dates.schema.json');
 module.exports.schema = require('./fuzzy-dates.schema.json');
 
 module.exports.convert = function(fd) {
+  var match;
+  var year;
+  var errorValue;
+
   if (!validate(fd))
     throw util.format('Invalid Fuzzy Date: %s', JSON.stringify(fd));
 
   var c = fd.constructor;
   if (c === String) {
-    if (fd.indexOf('-') > -1) {
+    if (isIso8601(fd)) {
       return [
         fd,
         fd
       ];
+    } else if (fd.indexOf('±') > -1) {
+      match = /(-?\d+)±(\d+)/.exec(fd);
+      year = parseInt(match[1]);
+      errorValue = parseInt(match[2]);
+      return yearJanDecRange(year - errorValue, year + errorValue);
     } else {
       return yearJanDecRange(fd, fd);
     }
@@ -34,21 +43,25 @@ module.exports.convert = function(fd) {
     }
   } else if (c === Object) {
     c = fd.date.constructor;
-    var match = /(\d+)(\w)/.exec(fd.error);
-    var errorValue = parseInt(match[1]);
+    match = /(-?\d+)(\w)/.exec(fd.error);
+    errorValue = parseInt(match[1]);
     var errorUnit = match[2];
 
-    if (c === String && fd.date.indexOf('-') > -1) {
+    if (c === String && isIso8601(fd.date)) {
       return [
         moment(fd.date).subtract(errorValue, errorUnit).format('YYYY-MM-DD'),
         moment(fd.date).add(errorValue, errorUnit).format('YYYY-MM-DD')
       ];
     } else {
-      var year = parseInt(fd.date);
+      year = parseInt(fd.date);
       return yearJanDecRange(year - errorValue, year + errorValue);
     }
   }
 };
+
+function isIso8601(str) {
+  return (str.match(/-/g) || []).length >= 2;
+}
 
 function yearJanDecRange(year1, year2) {
   return [
